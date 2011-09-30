cf96b7b88e0e3f156be1a7a9e090a10f11c193d3
Ken  Poland <ken.poland@gmail.com>
2011-09-30 16:35:06 +0100

    commit new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-FirstApp::Application.routes.draw do
+SecondApp::Application.routes.draw do
   resources :users
 
   # The priority is based upon order of creation:
@@ -50,7 +50,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-     root :to => "users#index"
+   root :to => "users#index"
 
   # See how all your routes lay out with "rake routes"
 
